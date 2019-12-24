c53c6a2104efef13fa6f20202dba9f6252065e7e
DmitrijPedan <dmitrijpedan84@gmail.com>
2019-12-24 11:10:36 +0200

    add user input

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('ITN Verification is started ...');
+cl('ITN Verification is started ...');
+function cl(inp) {
+    console.log(inp);
+}
+
+const getUserInput = () => {
+    inp = prompt('Введите 10 значный код ИНН:', '');
+    let spl = inp.split('');
+    let res;
+    inp == +inp && spl.length == 10 ? res = spl.map(el => +el) : alert('Неправильный ввод');
+    return res;
+};
+
+cl(getUserInput());
+
