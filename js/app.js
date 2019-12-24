08edf7f9e3cd3124e4a64eaa494bfa4fb22feb2e
DmitrijPedan <dmitrijpedan84@gmail.com>
2019-12-24 21:18:32 +0200

    add main function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-cl('ITN Verification is started ...');
-function cl(inp) {
-    console.log(inp);
-}
-
 const getUserInput = () => {
     inp = prompt('Введите 10 значный код ИНН:', '');
     let spl = inp.split('');
@@ -11,8 +6,7 @@ const getUserInput = () => {
     return res;
 };
 
-
-const check = (inp) => {
+const checkITN = (inp) => {
     let x = [-1,5,7,9,4,6,10,5,7,0];
     let summ = 0;
     let res;
@@ -23,12 +17,22 @@ const check = (inp) => {
     return res;
 }
 
-const bigFunction = () => check(getUserInput()) ? alert('Good') : alert ('Bad');
+const getUserGender = (inp) => {
+    let res;
+    inp[inp.length - 2] % 2 == 0 ? res = 'Женский' : res = 'Мужской';
+    return res;
+}
 
-bigFunction();
+const getUserData = (inp) => {
+    let days = +inp.splice(0,5).join('');
+    let date = new Date(days * 86400000);
+    let res = `${date.getFullYear() - 70}`; // `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()-70}`
+    return res;
+}
+ const mainFunction = () => {
+    let input = getUserInput()
+    checkITN(input) ? alert(`Код действителен. \nГод рождения: ${getUserData(input)} \nПол: ${getUserGender(input)}`) : alert(`Код не действителен.`);
+}
 
-// c = 240;
-// s = c % 10
-// k = (c % 11) % 10 ;
-// cl(k);
-
+ mainFunction();
+ history.back();
