8b3fdf4ad18e2dbebaf7a30920b924ea840b9908
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-01-26 15:00:59 +0200

    add HTML interface

--- js/app.js
+++ js/app.js
@@ -1,38 +1,52 @@
 const getUserInput = () => {
-    inp = prompt('Введите 10 значный код ИНН:', '');
-    let spl = inp.split('');
-    let res;
-    inp == +inp && spl.length == 10 ? res = spl.map(el => +el) : alert('Неправильный ввод');
-    return res;
+    let input = document.getElementById('inn-input').value;
+    input == +input && input.split('').length == 10 ? input = input.split('').map(el => +el) : alert('Неправильный ввод');
+    return input;
 };
 
-const checkITN = (inp) => {
-    let x = [-1,5,7,9,4,6,10,5,7,0];
-    let summ = 0;
-    let res;
-    for(let i = 0; i < inp.length; i++) {
-        summ += inp[i]*x[i];    
-    }
-    ((summ % 11) % 10) == inp[9] ? res = true : res = false;
-    return res;
+const getITNVerification = (inp) => {
+    let mask = [-1,5,7,9,4,6,10,5,7,0], result;
+    let summ = (inp.map((el, i) => el = el*mask[i])).reduce((acc=0, el) => acc+el);
+    ((summ % 11) % 10) == inp[9] ? result = true : result = false;
+    return result;
 }
 
 const getUserGender = (inp) => {
-    let res;
-    inp[inp.length - 2] % 2 == 0 ? res = 'Женский' : res = 'Мужской';
-    return res;
+    let result;
+    inp[inp.length - 2] % 2 == 0 ? result = 'Женский' : result = 'Мужской';
+    return result;
 }
 
 const getUserData = (inp) => {
-    let days = +inp.splice(0,5).join('');
+    const months = ['января','февраля','марта','апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    let days = +inp.splice(0,5).join('') - 25568;
     let date = new Date(days * 86400000);
-    let res = `${date.getFullYear() - 70}`; // `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()-70}`
+    let res = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()} года`;
     return res;
 }
- const mainFunction = () => {
-    let input = getUserInput()
-    checkITN(input) ? alert(`Код действителен. \nГод рождения: ${getUserData(input)} \nПол: ${getUserGender(input)}`) : alert(`Код не действителен.`);
+
+const createHTMLNode = (tag, attrs, inner) => {
+    const element = document.createElement(tag);
+    attrs.map(attr => {element.setAttribute(attr.name, attr.value.join(' '))});
+    inner
+        ?
+            Array.isArray(inner) ? inner.map(el => element.appendChild(el)):
+                element.innerHTML=inner
+                :null;
+    return element;
 }
 
- mainFunction();
- history.back();
+ const mainFunction = () => {
+    let input = getUserInput()
+    if (getITNVerification(input)) {
+        let message = createHTMLNode('div', [{name: 'class', value: ['alert', 'alert-success']}, {name: 'role', value: ['alert']}], `Код ${input.join('')} действителен. <br>Дата рождения: ${getUserData(input)}. <br>Пол: ${getUserGender(input)}`);
+        document.getElementById('app').prepend(message);
+    } else {
+        let message = createHTMLNode('div', [{name: 'class', value: ['alert', 'alert-danger']}, {name: 'role', value: ['alert']}], `Код ${input.join('')} не действителен.`);
+        document.getElementById('app').prepend(message);
+    }
+
+}
+
+document.getElementById('checkBtn').onclick =  mainFunction;
+
